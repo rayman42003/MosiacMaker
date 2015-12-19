2c10c0ebadf89451fb39c9822a7979bb61963d2c
Richmond Chang <changrichmond@gmail.com>
2015-12-18 23:08:16 -0800

    Basic averaging of color in a patch and setting the patch to that average.

--- MosiacMaker/MosiacMaker/Program.cs
+++ MosiacMaker/MosiacMaker/Program.cs
@@ -5,7 +5,7 @@ namespace MosiacMaker
 {
     class Program
     {
-        const int PATCH_SIZE = 1;
+        const int PATCH_SIZE = 8;
         static void Main(string[] args)
         {
             System.Console.WriteLine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
@@ -15,11 +15,29 @@ namespace MosiacMaker
             Bitmap bmp = (Bitmap)img;
 
 
-            for (int i = 0; i < bmp.Width/2; i += PATCH_SIZE)
+            for (int row = 0; row < bmp.Width; row += PATCH_SIZE)
             {
-                for (int j = 0; j < bmp.Height/2;  j += PATCH_SIZE)
+                for (int col = 0; col < bmp.Height;  col += PATCH_SIZE)
                 {
-                    bmp.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    int accR = 0;
+                    int accG = 0;
+                    int accB = 0;
+                    int count = 0;
+                    for (int i = row; i < row + PATCH_SIZE && i < bmp.Width; i++)
+                    {
+                        for (int j = col; j < col + PATCH_SIZE && j < bmp.Height; j++)
+                        {
+                            Color curr = bmp.GetPixel(i, j);
+                            accR += curr.R;
+                            accG += curr.G;
+                            accB += curr.B;
+                            ++count;
+                        }
+                    }
+                    Color avg = Color.FromArgb(accR / count, accG / count, accB / count);
+                    for (int i = row; i < row + PATCH_SIZE && i < bmp.Width; i++)
+                        for (int j = col; j < col + PATCH_SIZE && j < bmp.Height; j++)
+                            bmp.SetPixel(i, j, avg);
                 }
             }
 
