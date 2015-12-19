88b4bba64d8c94a1042ad47da13bbafcedf56d6a
Richmond Chang <changrichmond@gmail.com>
2015-12-18 22:58:44 -0800

    Figured out how to change values of an image.

--- MosiacMaker/MosiacMaker/Program.cs
+++ MosiacMaker/MosiacMaker/Program.cs
@@ -5,14 +5,25 @@ namespace MosiacMaker
 {
     class Program
     {
+        const int PATCH_SIZE = 1;
         static void Main(string[] args)
         {
             System.Console.WriteLine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location));
             string filename = @"E:\Dev\Projects\MosaicMaker\Example.jpg";
             ImgLoader loader = new FileImgLoader(filename);
             Image img = loader.Load();
+            Bitmap bmp = (Bitmap)img;
 
-            Application.Run(new MosaicForm(img));
+
+            for (int i = 0; i < bmp.Width/2; i += PATCH_SIZE)
+            {
+                for (int j = 0; j < bmp.Height/2;  j += PATCH_SIZE)
+                {
+                    bmp.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                }
+            }
+
+            Application.Run(new MosaicForm(bmp));
 
             System.Console.ReadKey();
         }
